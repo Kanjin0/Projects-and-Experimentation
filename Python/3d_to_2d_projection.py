8d028ee51b4b4cc9f8685983461e93427da7c7c2
Kanjin0 <rodrigo.jose.barata@gmail.com>
2026-07-02 16:13:55 +0100

    Changed Edges to be Obtained from a list of Faces instead of defining each one individually

--- Python/3d_to_2d_projection.py
+++ Python/3d_to_2d_projection.py
@@ -28,36 +28,29 @@ class Point3D(NamedTuple):
 
 cubeSize = 1
 half = cubeSize/2
-starting_plane = 1.5 #The model used assumes user plane is at z = 0
+
 
 solid = [
-    Point3D(half,half,half+starting_plane),
-    Point3D(-half,half,half+starting_plane),
-    Point3D(-half,-half,half+starting_plane),
-    Point3D(half,-half,half+starting_plane),
+    Point3D(half,half,half),
+    Point3D(-half,half,half),
+    Point3D(-half,-half,half),
+    Point3D(half,-half,half),
 
-    Point3D(half,half,-half+starting_plane),
-    Point3D(-half,half,-half+starting_plane),
-    Point3D(-half,-half,-half+starting_plane),
-    Point3D(half,-half,-half+starting_plane),
+    Point3D(half,half,-half),
+    Point3D(-half,half,-half),
+    Point3D(-half,-half,-half),
+    Point3D(half,-half,-half),
 ]
 
-lines = [   
-    (0,1),
-    (1,2),
-    (2,3),
-    (3,0),
+faces = [
+    (0,1,2,3), # Back Face (z = half + starting_plane) - Farthest from camera/user (at/close to z = 0) (Normal: +Z) - Away from camera
+    (5,4,7,6), # Front Face (z = -half + starting_plane) - Farthest from camera/user (Normal: -Z) - Towards camera
 
-    (4,5),
-    (5,6),
-    (6,7),
-    (7,4),
+    (1,5,6,2), # Left Face (x = -half) (Normal: -X)
+    (3,7,4,0), # Right Face (x = +half) (Normal: +X)
 
-    (0,4),
-    (1,5),
-    (2,6),
-    (3,7),
-
+    (4,5,1,0), # Top Face (y = +half) (Normal: +Y)
+    (3,2,6,7), # Bottom Face (y = -half) (Normal: -Y)
 ]
 
 #Draw rectangle with center at an x,y coord accounting for its size (so it's correctly placed there instead of placing the top right corner there) 
@@ -69,6 +62,17 @@ def point(point:Point2D):
 def line(point1:Point2D, point2:Point2D):
     size = 2
     pygame.draw.line(window,USE_COLOR, (point1.x, point1.y), (point2.x, point2.y),size)
+
+def build_wireframe_from_faces(faces:list):
+    edges = set()
+    for face in faces:
+        for i in range(len(face)):
+            v1 = face[i]
+            v2 = face[(i+1) % len(face)]
+
+            edge = tuple(sorted((v1,v2)))
+            edges.add(edge)
+    return list(edges)
 
 #Transform normal cartesian coordinates from -1 ... 1 -> 0 ... 2 -> 0 ... 1 -> 0 ... window_width/height
 def screenCoord(point:Point2D):
@@ -98,13 +102,19 @@ def rotation(point:Point3D, axis:int, angle):
 def gameloop():
     loop = True
     deltaTime = 1/FPS
-    deltaZ = 0
+    deltaZ = 2
     theta = pi*deltaTime/2
     angle = 0
+    lines = build_wireframe_from_faces(faces)
 
     while loop:
-        deltaZ = min(deltaZ + deltaTime/2,2)
         angle = angle + theta
+
+        #Some back and Forth Motion
+        if cos(angle/1.1) > 0:
+            deltaZ = min(deltaZ + deltaTime/2,3)
+        else:
+            deltaZ = max(deltaZ - deltaTime/2,-3)
         
         #Handle events (might implement drawing a solid by clicking to add vertexes)
         for event in pygame.event.get():
@@ -125,13 +135,13 @@ def gameloop():
             #Apply Transformations (need to be stacked, both for translation and rotation)
             rotations1 = rotation(
                 rotation(
-                    Point3D(p1.x, p1.y, p1.z),1,angle*0.),2,
-                    angle*0)
+                    Point3D(p1.x, p1.y, p1.z),1,angle),2,
+                    angle)
             rotations2 = rotation(
                 rotation(
-                    Point3D(p2.x, p2.y, p2.z),1,angle*0.),2,
-                    angle*0)
-            
+                    Point3D(p2.x, p2.y, p2.z),1,angle),2,
+                    angle)
+
             transformations1 = translation(rotations1,2,deltaZ)
             transformations2 = translation(rotations2,2,deltaZ)
 
@@ -139,7 +149,11 @@ def gameloop():
             point1 = screenCoord(projection(transformations1))
             point2 = screenCoord(projection(transformations2))
 
+
             line(point1,point2)
+
+        for face in faces:
+            pass
 
         pygame.display.update()
         clock.tick(FPS)
